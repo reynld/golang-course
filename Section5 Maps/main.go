b12b8039e8924682ec1508587de2c7d9046e4af6
Stublink <chrisrey95@gmail.com>
2018-12-25 22:08:49 -0500

    demonstarted iterating over a map as it is of reference type

--- Section5 Maps/main.go
+++ Section5 Maps/main.go
@@ -5,16 +5,23 @@ import "fmt"
 func main() {
 	// var colors map[string]string
 
-	colors := make(map[int]string)
+	// colors := make(map[int]string)
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	// colors[10] = "#ffffff"
 
-	colors[10] = "#ffffff"
+	// delete(colors, 10)
 
-	delete(colors, 10)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, " is ", hex)
+	}
 }
