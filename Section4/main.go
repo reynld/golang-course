ff5e4e3027f5f9b0cf02b5aae2aa3c5cca3fe0d2
Stublink <chrisrey95@gmail.com>
2018-12-25 21:21:16 -0500

    created nested struc's with struct declaration shorthand

--- Section4/main.go
+++ Section4/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
@@ -12,10 +18,21 @@ func main() {
 	// alex := person{"Alex", "Anderson"}
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
