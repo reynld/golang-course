74f48b7b75c0154e0c0bec1dc6337c44b6b5f11f
Stublink <chrisrey95@gmail.com>
2018-12-26 16:51:13 -0500

    added error handling for assigment3 + made it an executable

--- Section6InterfacesHTTP/Assigment3/main.go
+++ Section6InterfacesHTTP/Assigment3/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
 	args := os.Args
-	f, _ := os.Open(string(args[1]))
+	f, err := os.Open(string(args[1]))
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
 	io.Copy(os.Stdout, f)
 }
